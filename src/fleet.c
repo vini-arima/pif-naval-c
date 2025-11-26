225547f4de9ab666f726cd628aecc3c76cb233ce
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2025-11-26 16:37:18 -0300

    Adicionar codigo (implementar) - fleet.c / modificar fleet.h

---
+++ src/fleet.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "fleet.h" 
+
+void inicialize_fleet(Fleet *fleet){
+     fleet -> count = 6 ; 
+
+     fleet -> navios = malloc( 6 * sizeof(Navio)) ; 
+     
+     if (fleet->navios == NULL) {
+        return -1; 
+     }
+    
+    strcpy(fleet->navios[0].name, "Porta-Avioes");
+    fleet->navios[0].tamanho = 5;  
+    fleet->navios[0].dano = 0;   
+    fleet->navios[0].posicao = 0;  
+    
+   
+    strcpy(fleet->navios[1].name, "Encouracado");
+    fleet->navios[1].tamanho = 4;  
+    fleet->navios[1].dano = 0;   
+    fleet->navios[1].posicao = 0; 
+    
+
+    strcpy(fleet->navios[2].name, "Cruzador");
+    fleet->navios[2].tamanho = 3;  
+    fleet->navios[2].dano = 0;   
+    fleet->navios[2].posicao = 0 ; 
+
+    strcpy(fleet->navios[3].name, "Cruzador");
+    fleet->navios[3].tamanho = 3;  
+    fleet->navios[3].dano = 0;   
+    fleet->navios[3].posicao = 0;  
+
+    strcpy(fleet->navios[4].name, "Destroyer");
+    fleet->navios[4].tamanho = 2;  
+    fleet->navios[4].dano = 0;   
+    fleet->navios[4].posicao = 0; 
+    
+    strcpy(fleet->navios[5].name, "Destroyer");
+    fleet->navios[5].tamanho = 2;  
+    fleet->navios[5].dano = 0;   
+    fleet->navios[5].posicao = 0;  
+
+
+} 
+
+void free_board(Fleet *fleet){
+    if(fleet -> navios != NULL){
+        free(fleet -> navios);
+    }
+}
--- src/fleet.h
+++ src/fleet.h
@@ -5,22 +5,16 @@ typedef enum {ORIENT_H , ORIENT_V} Orientation ;
 
 typedef struct {
     char name[20]; // NOME
-    int length; // TAMANHO
-    int hits; // ACERTO
-    int placed; // POSICAO
+    int tamanho; // TAMANHO
+    int dano; // ACERTO
+    int posicao; // POSICAO
     Orientation orienta ;
-} Ship ;
+} Navio ;
 
 typedef struct{
-    Ship *navios ; 
+    Navio *navios ; 
     int count ; 
 } Fleet ;
 
-typedef enum {
-    navio_aviao,      // Porta-aviões (5 células)
-    navio_encouracado,   // Encouraçado (4 células)
-    navio_crusador,      // Cruzador (3 células)
-    navio_destroyer      // Destroyer (2 células)
-} ShipType ;
 
 #endif
